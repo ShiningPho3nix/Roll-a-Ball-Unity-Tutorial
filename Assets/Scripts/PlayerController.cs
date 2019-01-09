195a9db20dbf3bb543e3de82c79d80f57b9bef1f
ShiningPho3nix <son-goku__styler@online.de>
2019-01-09 18:08:56 +0100

    Score + Win, Abschluss des Roll a Ball Tutorials

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    private int score;
+    public Text scoreText;
+    public Text winText;
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        setScore();
+        winText.text = " ";
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -19,5 +26,25 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    // Destroy everything that enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            setScore();
+        }
+    }
+
+    private void setScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 16)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
