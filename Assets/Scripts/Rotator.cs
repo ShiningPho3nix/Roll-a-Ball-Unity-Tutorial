4b7b32e5a0422c200204524d2022131112e4b67a
ShiningPho3nix <son-goku__styler@online.de>
2019-01-09 16:23:46 +0100

    PickUp Objects
    Kamera Bewegung korrigiert, PickUp Objekte hinzugefügt, diesen Rotation gegeben

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateX;
+    public float rotateY;
+    public float rotateZ;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
+    }
+
+}
